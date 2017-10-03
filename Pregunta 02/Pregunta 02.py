32e47021f2bc8a3d994a6f9f697583f31ce14d58
cristzelada <cris_356_@hotmail.com>
2017-10-02 23:47:15 -0500

    p2

--- Pregunta 02/Pregunta 02.py
+++ Pregunta 02/Pregunta 02.py
@@ -1,29 +1,29 @@
 import sys
 
 class Entrada:
-    def __init__(self, entrada_id, pelicula_id, funcion, cantidad):
+    def __init__(self,entrada_id,pelicula_id,funcion,cantidad):
         self.entrada_id = entrada_id
         self.pelicula_id = pelicula_id
         self.funcion = funcion
         self.cantidad = cantidad
 
 class Pelicula:
-	instancia = None
-	def __init__(self, id, nombre):
+    instancia = None
+    def __init__(self,id,nombre):
             self.id = id
             self.nombre = nombre
-		
+        
 @classmethod
 def get_instance(cls):
     if cls.instancia == None:
         cls.instancia = Singleton()
     return cls.instancia
-		
+        
     
 
         
 class Funcion:
-    def _init__(self, id, hora, minuto):
+    def _init__(self,id,hora,minuto):
         self.id = id
         self.hora = hora
         self.minuto = minuto
@@ -45,14 +45,14 @@ class Cine:
         def guardar_entrada(self, Entrada):
             return self.listar_entradas(entrada.pelicula_id, entrada.funcion_id)
 
-			
+            
 class CineFactory:
 
-	def Escoger_Cine(self, tipo_Cine):
-		if tipo_Cine  == '1':
-			return CinePlaneta()
-		else:
-			return CineStark()
+    def Escoger_Cine(self,tipo_Cine):
+        if tipo_Cine  == '1':
+            return CinePlaneta()
+        else:
+            return CineStark()
 
 
 class CinePlaneta:
@@ -61,7 +61,7 @@ class CinePlaneta:
         peliculaHF = Pelicula.get_instance
         peliculaD = Pelicula.get_instance
         peliculaDeep = Pelicula.get_instance
-		
+        
     peliculaIT.id = 1
     peliculaIT.nombre = 'IT'
     peliculaHF.id = 2
@@ -87,7 +87,7 @@ class CineStark:
     def __init__(self):
         peliculaD = Pelicula.get_instance
         peliculaDeep = Pelicula.get_instance
-		
+        
     peliculaD.id = 1
     peliculaD.nombre = 'Desparecido'
     peliculaDeep.id = 2
@@ -103,11 +103,11 @@ class CineStark:
 class opcion_Principal:
     
     def obtener_opcion(self):
-	    print('Ingrese la opción que desea realizar')
-	    print('(1) Listar cines')
-	    print('(2) Listar cartelera')
-	    print('(3) Comprar entrada')
-	    print('(0) Salir')
+        print('Ingrese la opción que desea realizar')
+        print('(1) Listar cines')
+        print('(2) Listar cartelera')
+        print('(3) Comprar entrada')
+        print('(0) Salir')
 
 class opcion_Uno:
     
@@ -128,92 +128,93 @@ class opcion_Dos:
                 print('********************')
   
 class opcion_Tres:
-	def obtener_opcion(self):
+    def obtener_opcion(self):
                 print('********************')
                 print('COMPRAR ENTRADA')
                 print('Lista de cines')
                 print('1: CinePlaneta')
                 print('2: CineStark')
                 print('********************')
-			
-			
+            
+            
 class OpcionFactory:
 
-	def obtener_opcion(self, opcion):
-		if opcion  == '1':
-			return opcion_Uno()
-		elif opcion == '2':
-			return opcion_Dos()	
-		elif opcion == '3':
-			return opcion_Tres()
-		else:
-			return opcion_Principal()
+    def obtener_opcion(self, opcion):
+        if opcion  == '1':
+            return opcion_Uno()
+        elif opcion == '2':
+            return opcion_Dos() 
+        elif opcion == '3':
+            return opcion_Tres()
+        else:
+            return opcion_Principal()
 
 
 def main():
-            terminado = False;	
-            cinefactory = CineFactory()	
-            opcionfactory = OpcionFactory()
-	
+    terminado = False;  
+    cinefactory = CineFactory() 
+    opcionfactory = OpcionFactory()
+    
     while not terminado:
         
-        opcion = opcionfactory.obtener_opcion()		
-        opcion.obtener_opcion()		
-        opcion = input('Primero elija una opcion:')
-		
-	if opcion == '1':
-	    opcion = opcionfactory.obtener_opcion()
-            opcion.obtener_opcion()
-			
-	elif opcion == '2':
-	    opcion = opcionfactory.obtener_opcion()
-	    opcion.obtener_opcion()
-		
-            cine = input('Primero elija un cine:')
-            if cine == '1':
+            opcion = opcionfactory.obtener_opcion()     
+            opcion.obtener_opcion()     
+            opcion = input('Primero elija una opcion:')
+                    
+            if opcion == '1':
+                opcion = opcionfactory.obtener_opcion()
+                opcion.obtener_opcion()
+                            
+            elif opcion == '2':
+                opcion = opcionfactory.obtener_opcion()
+                opcion.obtener_opcion()
+                    
+                cine = input('Primero elija un cine:')
+                if cine == '1':
+                        # CinePlaneta
+                        cine = CinePlaneta()
+                elif cine == '2':
+                        cine = CineStark()
+
+                peliculas = cine.listar_peliculas()
+                print('********************')
+                for pelicula in peliculas:
+                        print("{}. {}".format(pelicula.id, pelicula.nombre))
+                print('********************')
+
+            elif opcion == '3':
+                opcion = opcionfactory.obtener_opcion()
+                opcion.obtener_opcion()
+                    
+                cine = input('Primero elija un cine:')
+                
+                if cine == '1':
                     # CinePlaneta
                     cine = CinePlaneta()
-            elif cine == '2':
+                elif cine == '2':
                     cine = CineStark()
 
-            peliculas = cine.listar_peliculas()
-            print('********************')
-            for pelicula in peliculas:
+                peliculas = cine.listar_peliculas()
+                
+                for pelicula in peliculas:
                     print("{}. {}".format(pelicula.id, pelicula.nombre))
-            print('********************')
-
-        elif opcion == '3':
-			opcion = opcionfactory.obtener_opcion()
-			opcion.obtener_opcion()
-		
-            cine = input('Primero elija un cine:')
-            if cine == '1':
-                # CinePlaneta
-                cine = CinePlaneta()
-            elif cine == '2':
-                cine = CineStark()
-
-            peliculas = cine.listar_peliculas()
+                pelicula_elegida = input('Elija pelicula:')
+                
+                #pelicula = obtener_pelicula(id_pelicula)
+                
+                print('Ahora elija la función (debe ingresar el formato hh:mm): ')
+                for funcion in cine.listar_funciones(pelicula_elegida):
+                    print('Función: {}'.format(funcion))
+                                    
+                funcion_elegida = input('Funcion:')
+                cantidad_entradas = input('Ingrese cantidad de entradas: ')
+                codigo_entrada = cine.guardar_entrada(pelicula_elegida, funcion_elegida, cantidad_entradas)
+                print('Se realizó la compra de la entrada. Código es {}'.format(codigo_entrada))
             
-            for pelicula in peliculas:
-                print("{}. {}".format(pelicula.id, pelicula.nombre))
-            pelicula_elegida = input('Elija pelicula:')
-            
-            #pelicula = obtener_pelicula(id_pelicula)
-            
-            print('Ahora elija la función (debe ingresar el formato hh:mm): ')
-            for funcion in cine.listar_funciones(pelicula_elegida):
-                print('Función: {}'.format(funcion))
-				
-            funcion_elegida = input('Funcion:')
-            cantidad_entradas = input('Ingrese cantidad de entradas: ')
-            codigo_entrada = cine.guardar_entrada(pelicula_elegida, funcion_elegida, cantidad_entradas)
-            print('Se realizó la compra de la entrada. Código es {}'.format(codigo_entrada))
-        
-	elif opcion == '0':
-            terminado = True
-        else:
-            print(opcion)
+            elif opcion == '0':
+                terminado = True
+            else:
+                print(opcion)
 
 
 
